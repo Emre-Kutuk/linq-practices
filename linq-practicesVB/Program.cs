54314a758a9a76c1ce0eef90a4495dc01b755791
Emre <kutukemre68@gmail.com>
2020-08-02 02:30:21 +0200

    Adding small code assignment

---
+++ linq-practicesVB/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_practicesVB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myprogram = new Program();
+            myprogram.Start();
+            Console.ReadKey();
+        }
+
+        void Start()
+        {
+            IEnumerable<int> vs = Enumerable.Range(1, 10);
+            Console.WriteLine(string.Join(",", MyFilter(vs)));
+        }
+        public static IEnumerable<int> MyFilter(IEnumerable<int> input)
+        {
+            var evens = input.Where(x => x % 2 == 0);
+            var squared = evens.Select(x => x * x);
+            var result = squared.Where(x => x < 50);
+
+            return result;
+        }
+
+        
+   
+
+}
+}
